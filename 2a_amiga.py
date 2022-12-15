f77d41263ca4a79fcb8780701f78bb42e2387a59
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-12-15 07:58:27 +0100

    Update 2a_amiga.py

--- 2a_amiga.py
+++ 2a_amiga.py
@@ -1,5 +1,11 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
+from direct.interval.IntervalGlobal import *
+from direct.filter.CommonFilters import CommonFilters
+
+loadPrcFileData("", "textures-power-2 none")
+loadPrcFileData("", "basic-shaders-only #t")
+
 base = ShowBase()
 ball = base.loader.loadModel('ball.bam')
 ball.reparentTo(base.render)
@@ -19,10 +25,22 @@ grid = base.loader.loadModel('grid.bam')
 grid.reparentTo(base.render)
 grid.setScale(0.005)
 grid.setPos(grid.getTightBounds()[0][0]/2, 2, 15)
-spot.node().setShadowCaster(True)
-base.render.setShaderAuto()
-base.render.setDepthOffset(-3)
+rotate_interval = LerpHprInterval(
+    nodePath=ball,
+    duration=5,
+    hpr=(90, -60, 360)
+)
+rotate_interval.loop()
+base.setFrameRateMeter(True)
+filters = CommonFilters(base.win, base.cam)
 
-spot.node().setShadowCaster(True, 8192, 8192)
+filters.setVolumetricLighting(
+    caster=spot,
+    numsamples=200,
+    density=0.1,
+    decay=0.98,
+)
+
+grid.setColorScale(1, 1, 1, 0)
 
 base.run()
