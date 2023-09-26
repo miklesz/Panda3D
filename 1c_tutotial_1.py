569cf3b5d22a5fe946fbb6935395fa39ccba8c9a
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2023-09-26 21:08:56 +0200

    Updated Python version and cleaned up scripts
    This commit updates the Python version from 3.9.7 to 3.11.5 in all relevant .ipynb files, bringing the notebooks in line with the latest Python release. Also cleaned up 1c_tutorial_1.py script by removing unnecessary lines about loading the model and accepting user inputs - these lines were found redundant and a more streamlined script helps in better readability and efficiency.

--- 1c_tutotial_1.py
+++ 1c_tutotial_1.py
@@ -1,19 +1,15 @@
 from direct.showbase.ShowBase import ShowBase
+
 from panda3d.core import WindowProperties
 
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
         properties = WindowProperties()
         properties.setSize(1000, 750)
         self.win.requestProperties(properties)
-        self.scene = \
-            self.loader.loadModel("models/environment")
-        self.scene.reparentTo(self.render)
-        self.disableMouse()
-
-        self.accept("escape", exit)
 
 
 app = MyApp()
