2375f56cb4eede166106acb9ce5c44c57db27391
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2023-09-29 00:11:57 +0200

    Improved tutorial slide structure and updated to recent build platforms
    Adjusted the content structure in the Panda3D tutorial 2 by changing slide_type to 'subslide' for improved clarity. Also updated the build platforms to 'manylinux2014_x86_64', to reflect necessity in working with the latest versions. The Python version in the notebooks has been updated to "3.11.5" for compatibility with latest features and libraries. In addition to this, numerous enhancements have been made to the 1c_tutorial_1.py script for better organization and efficiency. Furthermore, enabled Black formatter plugin in the Misc.xml file for better code quality assurance.

--- 1c_tutotial_1.py
+++ 1c_tutotial_1.py
@@ -1,16 +1,40 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from direct.interval.IntervalGlobal import *
+from direct.filter.CommonFilters import CommonFilters
 
-from panda3d.core import WindowProperties
+loadPrcFileData("", "textures-power-2 none")
+loadPrcFileData("", "basic-shaders-only #t")
+base = ShowBase()
+ball = base.loader.loadModel("ball.bam")
+ball.reparentTo(base.render)
+base.setBackgroundColor(0, 0, 0)
+ball.setPos(0, 17.5, 0)
+spotlight = Spotlight("spotlight")
+spot = base.render.attachNewNode(spotlight)
+spot.setPos(-25, -15, 30)
+spot.lookAt(ball)
+base.render.setLight(spot)
+ambient_light = AmbientLight("ambient light")
+ambient_light.setColor((0.1, 0.1, 0.1, 1))
+ambient = base.render.attachNewNode(ambient_light)
+base.render.setLight(ambient)
+ball.setHpr(90, -60, 0)
+grid = base.loader.loadModel("grid.bam")
+grid.reparentTo(base.render)
+grid.setScale(0.005)
+grid.setPos(grid.getTightBounds()[0][0] / 2, 2, 15)
+rotate_interval = LerpHprInterval(nodePath=ball, duration=5, hpr=(90, -60, 360))
+rotate_interval.loop()
+base.setFrameRateMeter(True)
+filters = CommonFilters(base.win, base.cam)
+filters.setVolumetricLighting(
+    caster=spot,
+    numsamples=200,
+    density=0.1,
+    decay=0.98,
+)
 
+grid.setColorScale(1, 1, 1, 0)
 
-class MyApp(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
-
-        properties = WindowProperties()
-        properties.setSize(1000, 750)
-        self.win.requestProperties(properties)
-
-
-app = MyApp()
-app.run()
+base.run()
